cd09ad06b7756d98aefd0036f685c03806dbf2b2
facio313 <faccio313@gmail.com>
2025-02-24 17:12:13 +0900

    20250224 지도 그리기 추가

--- frontend/indiv/src/views/map/Map.vue
+++ frontend/indiv/src/views/map/Map.vue
@@ -1,16 +1,18 @@
 <template>
     <h1>Map</h1>
     <div id="mapBox"></div>
+    <Draw :map="map"/>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import 'ol/ol.css';
-import { onMounted, ref } from 'vue';
 import { Map } from 'ol';
-import { Tile as TileLayer } from 'ol/layer';
-import { OSM } from 'ol/source';
 import { View } from 'ol';
+import { TileLayer } from 'ol/layer';
+import { OSM } from 'ol/source';
 import { fromLonLat } from 'ol/proj';
+import { Draw } from '@/components/map/Draw.vue';
 
 // onMounted 훅에서 지도 초기화
 onMounted(() => {
